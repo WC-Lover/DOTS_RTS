ed1fddb1bfc4cfd6a6c9e3a5b28138109e2bf0f1
Jegor <gaanjara3@gmail.com>
2025-03-15 23:30:08 +0200

    [UPDATE] DONE: - Added player movement. - Added player selection. - Added player selection area UI to show what area is selected.

---
+++ Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
@@ -0,0 +1,107 @@
+using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+using static UnitMoverAuthoring;
+
+public class UnitSelectionManager : MonoBehaviour
+{
+
+    public static UnitSelectionManager Instance;
+
+    public event Action OnSelectionAreaStart;
+    public event Action OnSelectionAreaEnd;
+
+    private Vector2 selectionStartMousePosition;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            selectionStartMousePosition = Input.mousePosition;
+            OnSelectionAreaStart?.Invoke();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            OnSelectionAreaEnd?.Invoke();
+
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            EntityQuery entityQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<Selected>().Build(entityManager);
+
+            // Mark all the Units as not selected
+            NativeArray<Entity> entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+            for (int i = 0; i < entityArray.Length; i++)
+            {
+                entityManager.SetComponentEnabled<Selected>(entityArray[i], false);
+            }
+
+            entityQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<LocalTransform, Unit>().WithPresent<Selected>().Build(entityManager);
+
+            Rect selectionAreaRect = GetSelectionAreaRect();
+
+            NativeArray<LocalTransform> localTransformArray = entityQuery.ToComponentDataArray<LocalTransform>(Allocator.Temp);
+            entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+
+            // Update all the Entites first
+            for (int i = 0; i < localTransformArray.Length; i++)
+            {
+                LocalTransform unitLocalTransform = localTransformArray[i];
+                Vector2 unitScreenPosition = Camera.main.WorldToScreenPoint(unitLocalTransform.Position);
+                if (selectionAreaRect.Contains(unitScreenPosition)) {
+                    // Unit is inside the selection area
+                    entityManager.SetComponentEnabled<Selected>(entityArray[i], true);
+                }
+            }
+            // Save updated info second
+            entityQuery.CopyFromComponentDataArray(localTransformArray);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 mouseWorldPosition = MouseWorldPosition.Instance.GetPosition();
+
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            EntityQuery entityQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<UnitMover, Selected>().Build(entityManager);
+
+            // Mark all the Units as not selected
+            NativeArray<Entity> entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+            NativeArray<UnitMover> unitMoverArray = entityQuery.ToComponentDataArray<UnitMover>(Allocator.Temp);
+            for (int i = 0; i < unitMoverArray.Length; i++)
+            {
+                UnitMover unitMover = unitMoverArray[i];
+                unitMover.targetPosition = mouseWorldPosition;
+                unitMoverArray[i] = unitMover;
+            }
+
+            entityQuery.CopyFromComponentDataArray(unitMoverArray);
+        }
+    }
+
+    public Rect GetSelectionAreaRect()
+    {
+        Vector2 selectionEndMousePosition = Input.mousePosition;
+
+        Vector2 lowerLeftCorner = new Vector2(
+            Mathf.Min(selectionStartMousePosition.x, selectionEndMousePosition.x),
+            Mathf.Min(selectionStartMousePosition.y, selectionEndMousePosition.y)
+            );
+
+        Vector2 upperRightCorner = new Vector2(
+            Mathf.Max(selectionStartMousePosition.x, selectionEndMousePosition.x),
+            Mathf.Max(selectionStartMousePosition.y, selectionEndMousePosition.y)
+            );
+
+        return new Rect(
+            lowerLeftCorner.x,
+            lowerLeftCorner.y,
+            upperRightCorner.x - lowerLeftCorner.x,
+            upperRightCorner.y - lowerLeftCorner.y
+            );
+    }
+}
